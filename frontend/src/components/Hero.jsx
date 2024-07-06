16216c424d6f929a1ec2626ddecde13b337ddc24
Shubham Saxena <117487370+shubz23@users.noreply.github.com>
2024-07-06 18:43:23 +0530

    Update Hero.jsx

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -7,12 +7,12 @@ const Hero = ({ title, imageUrl }) => {
         <div className="banner">
           <h1>{title}</h1>
           <p>
-            ZeeCare Medical Institute is a state-of-the-art facility dedicated
-            to providing comprehensive healthcare services with compassion and
+            MediExcellance is a state-of-the-art facility dedicated
+            to provide comprehensive healthcare services with compassion and
             expertise. Our team of skilled professionals is committed to
             delivering personalized care tailored to each patient's needs. At
-            ZeeCare, we prioritize your well-being, ensuring a harmonious
-            journey towards optimal health and wellness.
+            MediExcellance, we prioritize your well-being, ensuring a harmonious
+            the journey towards optimal health and wellness.
           </p>
         </div>
         <div className="banner">
